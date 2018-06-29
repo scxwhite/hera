99452b4143789d091d14737b8fa17869209d845f
pjx <lingxiao@2dfire.com>
2018-06-29 14:58:17 +0800

    delete

---
+++ hera-admin/src/test/java/com/dfire/exception/ExceptionTest.java
@@ -0,0 +1,72 @@
+package com.dfire.exception;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午5:02 2018/6/11
+ * @desc
+ */
+public class ExceptionTest {
+
+
+    boolean testEx() throws Exception {
+        boolean ret = true;
+        try {
+            ret = testEx1();
+        } catch (Exception e) {
+            System.out.println("testEx, catch exception");
+            ret = false;
+            throw e;
+        } finally {
+            System.out.println("testEx, finally; return value=" + ret);
+            return ret;
+        }
+    }
+
+    boolean testEx1() throws Exception {
+        boolean ret = true;
+        try {
+            ret = testEx2();
+            if (!ret) {
+                return false;
+            }
+            System.out.println("testEx1, at the end of try");
+            return ret;
+        } catch (Exception e) {
+            System.out.println("testEx1, catch exception");
+            ret = false;
+            throw e;
+        } finally {
+            System.out.println("testEx1, finally; return value=" + ret);
+            return ret;
+        }
+    }
+
+    boolean testEx2() throws Exception {
+        boolean ret = true;
+        try {
+            int b = 12;
+            int c;
+            for (int i = 2; i >= -2; i--) {
+                c = b / i;
+                System.out.println("i=" + i);
+            }
+            return true;
+        } catch (Exception e) {
+            System.out.println("testEx2, catch exception");
+            ret = false;
+            throw e;
+        } finally {
+            System.out.println("testEx2, finally; return value=" + ret);
+            return ret;
+        }
+    }
+
+    public static void main(String[] args) {
+        ExceptionTest testException1 = new ExceptionTest();
+        try {
+            testException1.testEx();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
