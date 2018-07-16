7bfb3a9539d77adaf9471b16b91fb9f548373857
pjx <lingxiao@2dfire.com>
2018-07-16 10:15:51 +0800

    去除无效引用

--- hera-admin/src/main/java/com/dfire/controller/LoginController.java
+++ hera-admin/src/main/java/com/dfire/controller/LoginController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 
 /**
