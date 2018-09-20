ce7e4711a45b947a1c84791d29d899655f7b264a
pjx <lingxiao@2dfire.com>
2018-09-20 17:25:50 +0800

    修改图标

--- hera-common/src/main/java/com/dfire/common/service/impl/HeraJobServiceImpl.java
+++ hera-common/src/main/java/com/dfire/common/service/impl/HeraJobServiceImpl.java
@@ -88,10 +88,10 @@ public class HeraJobServiceImpl implements HeraJobService {
         jobs.sort(Comparator.comparing(x -> x.getName().trim()));
         List<HeraJobTreeNodeVo> list = groups.stream()
                 .filter(group -> group.getExisted() == 1)
-                .map(g -> HeraJobTreeNodeVo.builder().id(g.getId() + "").parent(g.getParent() + "").directory(g.getDirectory()).isParent(true).name(g.getName() + "(" + g.getId() + ")").build()
+                .map(g -> HeraJobTreeNodeVo.builder().id(g.getId() + "-p").parent(g.getParent() + "-p").directory(g.getDirectory()).isParent(true).name(g.getName() + "(" + g.getId() + ")").build()
                 ).collect(Collectors.toList());
         List<HeraJobTreeNodeVo> jobList = jobs.stream()
-                .map(job -> HeraJobTreeNodeVo.builder().id(job.getId() + "").parent(job.getGroupId() + "").isParent(false).name(job.getName() + "(" + job.getId() + ")").build()).collect(Collectors.toList());
+                .map(job -> HeraJobTreeNodeVo.builder().id(job.getId() + "").parent(job.getGroupId() + "-p").isParent(false).name(job.getName() + "(" + job.getId() + ")").build()).collect(Collectors.toList());
         list.addAll(jobList);
         return list;
     }
