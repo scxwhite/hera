c8943ba99095665c5b85b9546ce519be39b559dd
xiaosuda <1142819049@qq.com>
2018-11-08 16:08:36 +0800

    切换为jdk自带调度池

--- hera-common/src/main/java/com/dfire/common/constants/Constants.java
+++ hera-common/src/main/java/com/dfire/common/constants/Constants.java
@@ -10,5 +10,8 @@ public class Constants {
     public final static String HERA_GROUP = "heraGroup";
 
     public final static String GROUP_PREFIX = "group_";
+    public final static String PRE_ENV = "pre";
+
+
 
 }
