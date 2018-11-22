275a4a6a82d56d175b280bc9a2b7ebb21f1ddb58
xiaosuda <1142819049@qq.com>
2018-11-22 18:50:08 +0800

    前端显示bug修复

--- hera-admin/src/main/resources/static/js/machineInfo.js
+++ hera-admin/src/main/resources/static/js/machineInfo.js
@@ -2,14 +2,14 @@ $(function () {
     $('#machineInfoMenu').addClass('active');
     var info = {};
     var ramOption = {
-        tooltip : {
+        tooltip: {
             formatter: "{a} <br/>{b} : {c}%"
         },
         series: [
             {
                 name: '业务指标',
                 type: 'gauge',
-                detail: {formatter:'{value}%'},
+                detail: {formatter: '{value}%'},
                 data: [{value: 50, name: '内存使用率'}]
             }
         ]
@@ -22,30 +22,31 @@ $(function () {
             type: 'get',
             success: function (data) {
                 //机器选择
-                for ( var i in data ){
-                    $('#machineList').append('<option value="'+i+'">'+i+'</option>');
+                for (var i in data) {
+                    $('#machineList').append('<option value="' + i + '">' + i + '</option>');
                 }
-                info=data;
+                info = data;
                 var machine = $('#machineList').val();
                 initInfo(data[machine]);
             }
         })
     }
-    function initInfo(machine){
+
+    function initInfo(machine) {
         console.log(machine)
         //系统概况
         ramOption.series[0].data[0].value = parseFloat(machine.osInfo.mem.toFixed(2));
-        var myChart=echarts.init(document.getElementById('ramGauge'));
+        var myChart = echarts.init(document.getElementById('ramGauge'));
         myChart.setOption(ramOption, true);
 
         $('#userPercent').easyPieChart({
             animate: 1000,
             barColor: '#011935',
-            onStep: function(from, to, percent) {
+            onStep: function (from, to, percent) {
                 $(this.el).find('.percent').text(Math.round(percent));
             },
-            size:200,
-            lineWidth:10,
+            size: 200,
+            lineWidth: 10,
             scaleColor: '#666'
         });
         $('#userPercent').data('easyPieChart').update(0);
@@ -54,11 +55,11 @@ $(function () {
         $('#SysPercent').easyPieChart({
             animate: 1000,
             barColor: '#009FAB',
-            onStep: function(from, to, percent) {
+            onStep: function (from, to, percent) {
                 $(this.el).find('.percent').text(Math.round(percent));
             },
-            size:200,
-            lineWidth:10,
+            size: 200,
+            lineWidth: 10,
             scaleColor: '#666'
         });
         $('#SysPercent').data('easyPieChart').update(0);
@@ -67,11 +68,11 @@ $(function () {
         $('#CPUPercent').easyPieChart({
             animate: 1000,
             barColor: '#1DB0B8',
-            onStep: function(from, to, percent) {
+            onStep: function (from, to, percent) {
                 $(this.el).find('.percent').text(Math.round(percent));
             },
-            size:200,
-            lineWidth:10,
+            size: 200,
+            lineWidth: 10,
             scaleColor: '#666'
         });
         $('#CPUPercent').data('easyPieChart').update(0);
@@ -80,11 +81,11 @@ $(function () {
         $('#SwapPercent').easyPieChart({
             animate: 1000,
             barColor: '#008891',
-            onStep: function(from, to, percent) {
+            onStep: function (from, to, percent) {
                 $(this.el).find('.percent').text(Math.round(percent));
             },
-            size:200,
-            lineWidth:10,
+            size: 200,
+            lineWidth: 10,
             scaleColor: '#666'
         });
         $('#SwapPercent').data('easyPieChart').update(0);
@@ -92,46 +93,59 @@ $(function () {
 
         //进程监控
         $("#processMonitor").bootstrapTable('destroy');
-        if(machine.processMonitor!==null){
+        if (machine.processMonitor !== null) {
             $('#processMonitor').bootstrapTable({
-                columns:[{
+                columns: [{
                     field: 'pid',
                     title: 'PID',
                     width: 150
-                },{
+                }, {
                     field: 'user',
                     title: 'USER',
                     width: 150
-                },{
+                }, {
                     field: 'viri',
                     title: 'VIRI',
                     width: 150
-                },{
+                }, {
                     field: 'res',
                     title: 'RES',
                     width: 150
-                },{
+                }, {
                     field: 'cpu',
                     title: 'CPU',
-                    formatter:function(value,row,index){
+                    formatter: function (value, row, index) {
                         value = parseFloat(value)
-                        value*=10;
-                        return '<div class="progress progress-xs"><div class="progress-bar progress-bar-primary progress-bar-striped" role="progressbar" aria-valuenow="'+value+'" aria-valuemin="0" aria-valuemax="10" style="width: '+value+'%"><span class="sr-only">40% Complete (success)</span></div></div>'
+                        if (value <= 60) {
+                            return '<div class="progress progress-xs"><div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="' + value + '" aria-valuemin="0" aria-valuemax="10" style="width: ' + value + '%"><span class="sr-only">40% Complete (success)</span></div></div>'
+
+                        } else if (value <= 80) {
+                            return '<div class="progress progress-xs"><div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="' + value + '" aria-valuemin="0" aria-valuemax="10" style="width: ' + value + '%"><span class="sr-only">40% Complete (success)</span></div></div>'
+                        } else {
+                            return '<div class="progress progress-xs"><div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="' + value + '" aria-valuemin="0" aria-valuemax="10" style="width: ' + value + '%"><span class="sr-only">40% Complete (success)</span></div></div>'
+                        }
                     },
                     width: 150
 
-                },{
+                }, {
                     field: 'mem',
                     title: 'MEM',
-                    formatter:function(value,row,index){
-                        return '<div class="progress progress-xs"><div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="'+value+'" aria-valuemin="0" aria-valuemax="100" style="width: '+value+'"><span class="sr-only">40% Complete (success)</span></div></div>'
+                    formatter: function (value, row, index) {
+                        if (value <= 60) {
+                            return '<div class="progress progress-xs"><div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="' + value + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + value + '%"><span class="sr-only">40% Complete (success)</span></div></div>'
+                        } else if (value <= 80) {
+                            return '<div class="progress progress-xs"><div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="' + value + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + value + '%"><span class="sr-only">40% Complete (success)</span></div></div>'
+                        } else {
+                            return '<div class="progress progress-xs"><div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="' + value + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + value + '%"><span class="sr-only">40% Complete (success)</span></div></div>'
+
+                        }
                     },
                     width: 150
-                },{
+                }, {
                     field: 'time',
                     title: 'TIME',
                     width: 150
-                },{
+                }, {
                     field: 'command',
                     title: 'COMMAND',
                     width: 150
@@ -141,13 +155,14 @@ $(function () {
         }
         $('#processMonitor').bootstrapTable('hideLoading');
         //机器信息
-        var machineInfo =machine.machineInfo;
+        var machineInfo = machine.machineInfo;
         $('#machineInfo').empty();
 
-        for(var i =0;machineInfo != null && i<machineInfo.length;i++){
-            $('#machineInfo').append('<div class="machine"><p class="filesystem">'+machineInfo[i].filesystem+'(已用'+machineInfo[i].used+'/可用'+machineInfo[i].avail+')</p><div class="progress progress-xs"><div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="'+machineInfo[i].use+'" aria-valuemin="0" aria-valuemax="100" style="width: '+machineInfo[i].use+'%"><span class="sr-only"></span></div></div></div>')
+        for (var i = 0; machineInfo != null && i < machineInfo.length; i++) {
+            $('#machineInfo').append('<div class="machine"><p class="filesystem">' + machineInfo[i].filesystem + '(已用' + machineInfo[i].used + '/可用' + machineInfo[i].avail + ')</p><div class="progress progress-xs"><div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="' + machineInfo[i].use + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + machineInfo[i].use + '%"><span class="sr-only"></span></div></div></div>')
         }
     }
+
     initMachine();
     $('#machineList').change(function (e) {
         e.stopPropagation();
