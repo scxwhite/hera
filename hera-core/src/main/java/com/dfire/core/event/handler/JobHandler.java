6cc562a7862e3355b14da14fcdce486c169309c2
xiaosuda <1142819049@qq.com>
2019-01-15 10:36:23 +0800

    时区注释

--- hera-core/src/main/java/com/dfire/core/event/handler/JobHandler.java
+++ hera-core/src/main/java/com/dfire/core/event/handler/JobHandler.java
@@ -390,7 +390,8 @@ public class JobHandler extends AbstractHandler {
             JobDetail jobDetail = JobBuilder.newJob(HeraQuartzJob.class).withIdentity(jobKey).build();
             jobDetail.getJobDataMap().put("actionId", heraActionVo.getId());
             jobDetail.getJobDataMap().put("dispatcher", dispatcher);
-            CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(heraActionVo.getCronExpression().trim());
+            //TODO  根据任务区域加时区
+            CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(heraActionVo.getCronExpression().trim())/*.inTimeZone()*/;
             CronTrigger trigger = TriggerBuilder.newTrigger().withIdentity(actionId, Constants.HERA_GROUP).withSchedule(scheduleBuilder).build();
             masterContext.getQuartzSchedulerService().getScheduler().scheduleJob(jobDetail, trigger);
             ScheduleLog.info("--------------------------- 添加自动调度成功:{}--------------------------", heraActionVo.getId());
