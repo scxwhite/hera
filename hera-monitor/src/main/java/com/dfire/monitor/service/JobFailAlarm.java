385acca361c6ce0294b0a2be1dd8ceee52eb279b
xiaosuda <1142819049@qq.com>
2019-02-26 15:17:59 +0800

    注释

--- hera-monitor/src/main/java/com/dfire/monitor/service/JobFailAlarm.java
+++ hera-monitor/src/main/java/com/dfire/monitor/service/JobFailAlarm.java
@@ -10,6 +10,7 @@ public interface JobFailAlarm {
 
     /**
      *  任务失败告警接口，自己可以自定义实现 默认:com.dfire.monitor.service.impl.EmailJobFailAlarm
+     *  一定要把实现类使用spring管理
      * @param actionId  hera_action.id字段
      */
     void alarm(String actionId);
