3783be6abb0f5121d8f9c20ba39c97088257efc9
苏承祥 <1142819049@qq.com>
2019-03-25 16:26:45 +0800

    完成日志

---
+++ hera-admin/src/main/java/com/dfire/config/HeraRunListener.java
@@ -0,0 +1,60 @@
+package com.dfire.config;
+
+import com.dfire.common.util.ActionUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.Date;
+
+/**
+ * desc:
+ * hera启动监听
+ *
+ * @author scx
+ * @create 2019/03/25
+ */
+public class HeraRunListener implements SpringApplicationRunListener {
+
+
+    private Date startTime;
+
+    private SpringApplication application;
+    private String[] args;
+
+
+    public HeraRunListener(SpringApplication application, String[] args) {
+        this.application = application;
+        this.args = args;
+    }
+
+    @Override
+    public void starting() {
+        startTime = new Date();
+        System.out.println("==========开始启动: " + ActionUtil.getDefaultFormatterDate(startTime));
+    }
+
+    @Override
+    public void environmentPrepared(ConfigurableEnvironment environment) {
+
+    }
+
+    @Override
+    public void contextPrepared(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void contextLoaded(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void finished(ConfigurableApplicationContext context, Throwable exception) {
+
+        Date endTime = new Date();
+
+        System.out.println("==========启动完成: " + ActionUtil.getDefaultFormatterDate(endTime) + "; 共花费: " + (endTime.getTime() - startTime.getTime()) + "ms");
+    }
+}
