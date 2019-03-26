6a62e74c2dfbda9016aa2a6789fe57c9d2e8ea62
苏承祥 <1142819049@qq.com>
2019-03-26 20:48:31 +0800

    密钥路径修改

--- hera-core/src/main/java/com/dfire/core/job/AbstractJob.java
+++ hera-core/src/main/java/com/dfire/core/job/AbstractJob.java
@@ -61,7 +61,7 @@ public abstract class AbstractJob implements Job {
         if (HeraGlobalEnvironment.isEmrJob()) {
             //这里的参数使用者可以自行修改，从hera机器上向emr集群分发任务
             command.append("ssh -o StrictHostKeyChecking=no").append(Constants.BLANK_SPACE);
-            command.append("-i /home/docker/deploy/apps/hera_tuya/conf/bigdata.pem").append(Constants.BLANK_SPACE);
+            command.append("-i /home/docker/conf/bigdata.pem").append(Constants.BLANK_SPACE);
             command.append("hadoop@").append(EmrUtils.getIp()).append(Constants.BLANK_SPACE).append("\\").append(Constants.NEW_LINE);
             command.append("<< eeooff").append(Constants.NEW_LINE);
             switch (runTypeEnum) {
