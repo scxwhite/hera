15bb40460e081857db1d1ac2a62d5a3e4670d353
苏承祥 <1142819049@qq.com>
2019-11-12 19:53:20 +0800

    2.3升级2.4sql

--- update/sql/2.3->2.4/update.sql
+++ update/sql/2.3->2.4/update.sql
@@ -1,23 +1,20 @@
+#权限表逻辑移除
+alter table hera_permission add columns `is_valid` tinyint(1) NOT NULL DEFAULT '1';
 
-#添加记录表
-CREATE TABLE IF NOT EXISTS `hera_record`
+#增加sso用户表
+CREATE TABLE if not exists `hera_sso`
 (
-  `id`           int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `type`         tinyint(4)       NOT NULL DEFAULT '0' COMMENT '日志类型：比如编辑、更新',
-  `content`      mediumtext COMMENT '脚本的变更前的内容',
-  `log_type`     varchar(15)      NOT NULL DEFAULT '-1' COMMENT '记录的日志的类型 比如：任务/组',
-  `log_id`       int(11)          NOT NULL DEFAULT '-1' COMMENT '任务id/组id',
-  `gmt_create`   bigint(13)       NOT NULL DEFAULT '1557814087800' COMMENT '创建时间',
-  `gmt_modified` bigint(13)       NOT NULL DEFAULT '1557814087800' COMMENT '同步专用字段',
-  `sso`          varchar(32)      NOT NULL DEFAULT 'hera' COMMENT '用户名称',
-  `gId`          int(11)          NOT NULL DEFAULT '-1' COMMENT '组id',
-  PRIMARY KEY (`id`)
+  `id`           int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name`         varchar(16)      NOT NULL DEFAULT '' COMMENT '用户名',
+  `password`     varchar(32)      NOT NULL DEFAULT '' COMMENT '密码',
+  `gid`          int(11)          NOT NULL DEFAULT '0' COMMENT '组id，对应hera_user的主键',
+  `phone`        char(11)         NOT NULL DEFAULT '' COMMENT '手机号',
+  `email`        varchar(52)      NOT NULL DEFAULT '' COMMENT '邮箱',
+  `job_number`   char(5)          NOT NULL DEFAULT '' COMMENT '工号',
+  `gmt_modified` bigint(20)       NOT NULL DEFAULT '0' COMMENT '编辑时间',
+  `is_valid`     tinyint(1)       NOT NULL DEFAULT '0' COMMENT '是否有效：0:无效：1：有效',
+  `gmt_create`   datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unq_name` (`name`) USING BTREE
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 37
-  DEFAULT CHARSET = utf8mb4 COMMENT ='日志记录表';
-
-#添加逻辑删除字段
-alter table hera_job add column `is_valid` tinyint(1) DEFAULT '1' COMMENT '任务是否删除';
-
-
-
+  DEFAULT CHARSET = utf8mb4 COMMENT ='sso用户表';
