15cfc417e355bfc946c6468586954e3915fc780b
苏承祥 <1142819049@qq.com>
2020-05-21 14:29:38 +0800

    合并内部分布式所

--- hera-core/src/main/java/com/dfire/core/lock/DistributeLock.java
+++ hera-core/src/main/java/com/dfire/core/lock/DistributeLock.java
@@ -27,23 +27,18 @@ import java.util.concurrent.TimeUnit;
 public class DistributeLock {
 
 
+    @Getter
+    private static boolean isMaster = false;
+    private final long timeout = 1000 * 60 * 5L;
+    private final String ON_LINE = "online";
     @Autowired
     private HeraHostRelationService hostGroupService;
     @Autowired
     private HeraLockService heraLockService;
-
     @Autowired
     private WorkClient workClient;
-
     @Autowired
     private HeraSchedule heraSchedule;
-
-    private final long timeout = 1000 * 60 * 5L;
-
-    private final String ON_LINE = "online";
-
-    @Getter
-    private static boolean isMaster = false;
 
     @PostConstruct
     public void init() {
@@ -51,53 +46,53 @@ public class DistributeLock {
     }
 
     public void checkLock() {
-        HeraLock heraLock = heraLockService.findBySubgroup(ON_LINE);
-        if (heraLock == null) {
-            Date date = new Date();
-            heraLock = HeraLock.builder()
-                    .id(1)
-                    .host(WorkContext.host)
-                    .serverUpdate(date)
-                    .subgroup(ON_LINE)
-                    .gmtCreate(date)
-                    .gmtModified(date)
-                    .build();
-            Integer lock = heraLockService.insert(heraLock);
-            if (lock == null || lock <= 0) {
-                return;
-            }
-        }
-
-        if (isMaster = WorkContext.host.equals(heraLock.getHost().trim())) {
-            heraLock.setServerUpdate(new Date());
-            heraLockService.update(heraLock);
-            HeraLog.info("hold lock and update time");
-            heraSchedule.startup();
-        } else {
-            long currentTime = System.currentTimeMillis();
-            long lockTime = heraLock.getServerUpdate().getTime();
-            long interval = currentTime - lockTime;
-            if (interval > timeout && isPreemptionHost()) {
-                Date date = new Date();
-                Integer lock = heraLockService.changeLock(WorkContext.host, date, date, heraLock.getHost());
-                if (lock != null && lock > 0) {
-                    ErrorLog.error("master 发生切换,{} 抢占成功", WorkContext.host);
-                    heraSchedule.startup();
-                    heraLock.setHost(WorkContext.host);
-                    //TODO  接入master切换通知
-                } else {
-                    HeraLog.info("master抢占失败，由其它worker抢占成功");
-                }
-            } else {
-                //非主节点，调度器不执行
-                heraSchedule.shutdown();
-            }
-        }
-        workClient.init();
         try {
+            HeraLock heraLock = heraLockService.findBySubgroup(ON_LINE);
+            if (heraLock == null) {
+                Date date = new Date();
+                heraLock = HeraLock.builder()
+                        .id(1)
+                        .host(WorkContext.host)
+                        .serverUpdate(date)
+                        .subgroup(ON_LINE)
+                        .gmtCreate(date)
+                        .gmtModified(date)
+                        .build();
+                Integer lock = heraLockService.insert(heraLock);
+                if (lock == null || lock <= 0) {
+                    return;
+                }
+            }
+
+            if (isMaster = WorkContext.host.equals(heraLock.getHost().trim())) {
+                heraLock.setServerUpdate(new Date());
+                heraLockService.update(heraLock);
+                HeraLog.info("hold lock and update time");
+                heraSchedule.startup();
+            } else {
+                long currentTime = System.currentTimeMillis();
+                long lockTime = heraLock.getServerUpdate().getTime();
+                long interval = currentTime - lockTime;
+                if (interval > timeout && isPreemptionHost()) {
+                    Date date = new Date();
+                    Integer lock = heraLockService.changeLock(WorkContext.host, date, date, heraLock.getHost());
+                    if (lock != null && lock > 0) {
+                        ErrorLog.error("master 发生切换,{} 抢占成功", WorkContext.host);
+                        heraSchedule.startup();
+                        heraLock.setHost(WorkContext.host);
+                        //TODO  接入master切换通知
+                    } else {
+                        HeraLog.info("master抢占失败，由其它worker抢占成功");
+                    }
+                } else {
+                    //非主节点，调度器不执行
+                    heraSchedule.shutdown();
+                }
+            }
+            workClient.init();
             workClient.connect(heraLock.getHost().trim());
         } catch (Exception e) {
-            ErrorLog.error("连接master失败", e);
+            ErrorLog.error("检测锁异常", e);
         }
     }
 
